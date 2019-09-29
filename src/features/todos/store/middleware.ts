5981b67780bc1d50ce436b4a95a42c7382f6c3be
Sergey <iamgreyy@gmail.com>
2019-09-29 19:47:05 +0500

    add action to middleware

--- src/features/todos/store/middleware.ts
+++ src/features/todos/store/middleware.ts
@@ -1,7 +1,11 @@
 import { TOwnMiddleware } from 'features/store'
 
 export const todosMiddleware: TOwnMiddleware = ({ getState }) => (next) => (action) => {
-  if (action.type === 'ADD_TASK' || action.type === 'DELETE_TASK') {
+  if (
+    action.type === 'ADD_TASK' ||
+    action.type === 'DELETE_TASK' ||
+    action.type === 'TOGGLE_TODO_DONE_ACTION'
+  ) {
     next(action)
 
     const { todos } = getState()
